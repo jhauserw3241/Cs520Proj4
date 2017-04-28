fe61169210b102c6f71b52ede9c7f78879c9fb7d
Lauren Lynch <lklynch@viper.cs.ksu.edu>
2017-04-28 00:18:36 -0500

    got most of the issues with pthread

--- hw5/3way-openmp/base.c
+++ hw5/3way-openmp/base.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <pthread.h>
+
 #include "base.h"
 
 #define CHUNK_SIZE 3
 #define TERMS 3
-#define NUM_THREADS ARRAY_SIZE*(ARRAY_SIZE/CHUNK_SIZE)
-
-pthread_mutex_t search_mutex;
 
 int main(int argc, char *argv[])
 {	
@@ -46,64 +43,33 @@ int main(int argc, char *argv[])
 	source_array[i][0] = NULL;
 
 	PrintResults();
-	
-	pthread_mutex_destroy(&search_mutex);
-	printf("Program completed. Exiting.\n");
-	pthread_exit(NULL);
 
 	return 0;
 }
 
-void *SearchForTerm(void *args) {
+int SearchForTerm(char term[], int source_index, int start, int end) {
 	int j = 0;
-	arg_t argt = *((arg_t *)args);
-	char *term = argt.term;
-	int cID = argt.chunk_ID;
-	int diff = argt.diff;
-	int source_index = argt.source_index;
-	int start, end;
-	
-	start = cID * CHUNK_SIZE;
-
-	if(diff <= 0){
-		end = CHUNK_SIZE + start;
-	}
-	else{
-		end = diff + start;
-	}
 
 	for(int i = start; (input_array[i][0]) && (i < end); i++) {
 		if(strstr(input_array[i], term)) {
-			pthread_mutex_lock(&search_mutex);
 			if((output_array[source_index][0] != '\0') || (j != 0)) {
 				sprintf(output_array[source_index], "%s, %i", output_array[source_index], i + 1);
 			}
 			else {
 				sprintf(output_array[source_index], "%i", i + 1);
 			}
-			pthread_mutex_unlock(&search_mutex);
 			j++;
 		}
 	}
-	pthread_exit(NULL);
 }
 
 int ReadInputDataIntoArray(char file[]) {
 	FILE *f;
 	char temp[STRING_SIZE];
-	int rc;
 	int i = 0;
 	int count = 0;
-	int cID = 0;
 	int terms = sizeof(source_array) / STRING_SIZE;
-	pthread_t input_threads[NUM_THREADS];
-	pthread_attr_t attr;
-	void *status;
-	arg_t args;
-	int *arg_mallocs = malloc(sizeof(arg_t));
 
-	pthread_attr_init(&attr);
-	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
 	f = fopen(file, "rt");
 	if(f == NULL)
@@ -116,18 +82,8 @@ int ReadInputDataIntoArray(char file[]) {
 
 		if((count + 1) == CHUNK_SIZE) {
 			for(int j = 0; j < terms; j++) {
-				args.chunk_ID = cID;
-				args.term = source_array[j];
-				args.source_index = j;
-				int * arg_mal = malloc(sizeof(arg_t));
-				rc = pthread_create(&input_threads[cID+j],&attr, SearchForTerm,(void *)args);
-				if(rc){
-					printf("ERROR; return code from pthread_create() is %d\n",rc);
-					exit(-1);
-				}
-				free(arg_mal);
+				SearchForTerm(source_array[j], j, (i + 1) - CHUNK_SIZE, (i + 1));
 			}
-			cID++;
 			count = 0;
 		}
 		else {
@@ -139,26 +95,8 @@ int ReadInputDataIntoArray(char file[]) {
 
 	int dif = i % CHUNK_SIZE;
 	if(dif != 0) {
-		cID++;
 		for(int j = 0; j < terms; j++) {
-			args.diff = dif;
-			args.chunk_ID = cID;
-			args.term = source_array[j];
-			rc = pthread_create(&input_threads[cID+1+j],&attr, SearchForTerm, (void *) args);
-			if(rc){
-				printf("ERROR; return code from pthread_create() is %d\n",rc);
-				exit(-1);
-			}
-		}
-	}
-
-	pthread_attr_destroy(&attr);
-	for(int x = 0; x < NUM_THREADS; x++){
-		rc = pthread_join(input_threads[x], &status);
-		if(rc){
-			printf("ERROR; return code from pthread_join() is %d\n",rc);
-			exit(-1);
-
+			SearchForTerm(source_array[j], j, i - dif, i);
 		}
 	}
 
